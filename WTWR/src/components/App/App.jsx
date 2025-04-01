c051de328b41f5260221a89d5f2e01ba1114060f
Jose Canela <jose.canela1693@gmail.com>
2025-03-31 21:19:54 -0400

    create form modal and style

--- WTWR/src/components/App/App.jsx
+++ WTWR/src/components/App/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { Header } from "../Header/Header";
 import { Main } from "../Main/Main";
 import { Footer } from "../Footer/Footer";
+import { ModalWithForm } from "../ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "hot" });
@@ -15,6 +16,50 @@ function App() {
         <Main weatherData={weatherData} />
         <Footer />
       </div>
+      <ModalWithForm buttonText="Add garment" title="New Garment">
+        <label htmlFor="name" className="modal__label modal__label_type_normal">
+          Name{""}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label
+          htmlFor="imgUrl"
+          className="modal__label modal__label_type_normal"
+        >
+          Image{""}
+          <input
+            type="url"
+            className="modal__input"
+            id="imgUrl"
+            placeholder="Image URL"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input type="radio" className="modal__radio-input" id="hot" />
+            Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input type="radio" className="modal__radio-input" id="warm" />
+            Warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input type="radio" className="modal__radio-input" id="cold" />
+            Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
